c8c85b0f97e2d2a9346a7a97cd49a62eedc1408e
Lauro Fraga <fragalauro12@gmail.com>
2024-03-14 16:05:56 -0300

    Update Password_Generator.py
    Adição de interface gráfica utilizando a biblioteca PySimpleGUI

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,24 +1,35 @@
-import random
+import PySimpleGUI as sg
 import string
+import random
 
-def password_generator(len = 8):
+layout = [
+    [sg.Text("E-mail/Usuário", size=(10, 1)),
+     sg.Input(key="usuario", size=(10, 1))],
+    [sg.Text("Quantidade de dígitos"), sg.Combo(values=list(range(30)), key="total_chars", 
+                                                default_value=1, size=(3, 1))],
+    [sg.Output(size=(40, 5))],
+    [sg.Button("Gerar senha")]
+]
+
+window = sg.Window("Password Generator", layout)
+
+def password_generator(len=8, options=None):
     asccii_options = string.ascii_letters
     number_options = string.digits
     punctuation_options = string.punctuation
     options = asccii_options + number_options + punctuation_options
     
     password_user = ""
-    for i in range(0, len):
+    for i in range(len):
         character = random.choice(options)
-        password_user = password_user + character
-    return print("Senha gerada : /n" + password_user)
+        password_user += character
+    return password_user
 
-user_choice = input("Quantos digitos você deseja utilizar? ")
-if user_choice.isdigit():
-    user_choice = int(user_choice)
-else:
-    print("Entrada invalida. ")
-    quit()
-    
-    
-password_generator(len=user_choice)
+while True:
+    event, value = window.read()
+    if event == sg.WINDOW_CLOSED:
+        break
+    if event == "Gerar senha":
+        user_choice = int(value["total_chars"])
+        new_password = password_generator(user_choice)
+        print(new_password)
